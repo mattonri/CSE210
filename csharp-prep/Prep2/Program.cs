5e0030c925bc7c89f9bdcc791fe8fd2020ca6427
Matthew Talbert <43828089+mattonri@users.noreply.github.com>
2024-02-26 12:08:55 -0700

    Finished C # Prep 1 & 2 exercise

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,46 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please input your grade as a percentage:");
+        string mrtuserInput = Console.ReadLine();
+        float grade = float.Parse(mrtuserInput);
+        string letterGrade = "unassigned";
+        if (grade >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (grade < 60)
+        {
+            letterGrade = "F";
+        }
+        else if (grade < 0 || grade > 120)
+        {
+            letterGrade = "Invalid Input";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congrats! You passed!");
+        }
+        else
+        {
+            Console.WriteLine($"Sorry, You didn't quite pass.");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox  World!");
+        int x = 5;
+        int y = 6;
+        if (x > y) 
+        {
+            Console.WriteLine($"{x} is greater than y!");
+        }
+        else if (x == y || y == x)
+        {
+            Console.WriteLine($"Sorry, {x} is equal to {y}");
+        }
+        // You could combine statements. (this one below is redundant)
+        else if (x < y && !( x > y) && !(x == y))
+        {
+            Console.WriteLine($"Sorry, {x} is larger than {y}");
+        }
     }
 }
