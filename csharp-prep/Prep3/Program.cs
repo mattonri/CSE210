ab09d8624dff2cf51743b1a4415173f9289bb6e9
Matthew Talbert <43828089+mattonri@users.noreply.github.com>
2024-02-26 20:28:09 -0700

    DId Prepare #3 activity

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("Please Guess a number: ");
+        int guess = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int randomNumber = randomGenerator.Next(1,100);
+        while (guess != randomNumber)
+        {
+            if (guess > randomNumber) 
+            {
+            Console.WriteLine($"Sorry! It's Try a number a little lower!");
+            }
+            else 
+            {
+            Console.WriteLine($"Sorry! It's Try a number a little higher!");
+            }
+            Console.Write($"What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("You got it!");
     }
 }
